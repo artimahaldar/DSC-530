f954ae3ecafc8427757979ab0fbc1970e2e8c7f8
artimahaldar <amahaldar@my365.bellevue.edu>
2021-12-11 12:39:28 -0600

    Week 2 Assignment

---
+++ Week2/mycodeAMahaldar.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 19:42:26 2021
+
+@author: Arti Mahaldar
+"""
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print("Hello World!")
+
+# Add two numbers
+a = 4
+b = 5
+print(a + b);
+
+# Subtract a number from another number
+a = 6
+b = 2
+print(a - b);
+
+# Multiply two numbers
+a = 5
+b = 7
+print(a * b);
+
+# Divide between two numbers
+a = 10
+b = 2
+print(a/b);
+
+# Concatenate two strings together (any words)
+a = 'Welcome '
+b = 'Python'
+print(a + b);
+
+# Create a list of 4 items (can be strings, numbers, both)
+list = ['Test','class',5, 20]
+print(list);
+
+# Append an item to your list (again, can be a string, number)
+list.append('Planet')
+print(list);
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+test_tuple = ('Maths','Physics', 50 , 60)
+print(test_tuple);
+
+
+
